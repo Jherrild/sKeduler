5a7ddfcb4f2843ea62dec12b0b257b61a519d7a7
Jesten Herrild <jherrild@expedia.com>
2018-03-24 16:21:15 -0700

    Got application connecting to local database spun up with docker

--- sKeduler-backend/src/main/java/com/jherrild/server/ScheduleController.java
+++ sKeduler-backend/src/main/java/com/jherrild/server/ScheduleController.java
@@ -1,18 +1,21 @@
 package com.jherrild.server;
 
 import com.jherrild.server.entity.Calendar;
-import com.jherrild.server.entity.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.StreamSupport;
 
 /**
  * @author jestenh@gmail.com
@@ -24,7 +27,7 @@ import java.util.HashMap;
 public class ScheduleController {
     @Autowired
     CalendarRepository calendarRepository;
-    private HashMap<String, Calendar> calendars = new HashMap<>();
+    private Calendar badCalendar = new Calendar();
 
     /**
      * Test endpoint to see if controller is being mapper and served properly
@@ -43,29 +46,43 @@ public class ScheduleController {
      */
     @RequestMapping(value="/calendars", method = RequestMethod.GET)
     public @ResponseBody
-    HashMap<String, Calendar> getAllCalendars() {
+    Map<String, Calendar> getAllCalendars() {
+        ConcurrentHashMap<String, Calendar> calendars = new ConcurrentHashMap<>();
+        StreamSupport.stream(calendarRepository.findAll().spliterator(), true)
+                .forEach(c -> calendars.put(c.getName(), c));
+
         return calendars;
     }
 
     /**
-     * Returns a Calendar if one already exists with the specified name- otherwise creates one
+     * Returns a Calendar if one already exists with the specified name
      * @return Calendar
      */
     @RequestMapping(value="/calendar/{calendarName}", method = RequestMethod.GET)
     public @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
     Calendar getCalendar(@PathVariable String calendarName) {
-        if(calendars.containsKey(calendarName)) {
-            return calendars.get(calendarName);
-        }else {
+        return calendarRepository.findByName(calendarName);
+    }
+
+    @RequestMapping(value="/calendar/create/{calendarName}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.CREATED)
+    public @ResponseBody
+    Calendar createCalendar(@PathVariable String calendarName) {
+        Calendar found = getCalendar(calendarName);
+        if(found == null) {
             Calendar createdCalendar = new Calendar(calendarName, new ArrayList<>());
-            calendars.put(calendarName, createdCalendar);
+            calendarRepository.save(createdCalendar);
             return createdCalendar;
+        }else {
+            return found;
         }
     }
 
+    /*
     @RequestMapping(value="calendar/{calendarName}/events/{newEvent}", method = RequestMethod.GET)
     public @ResponseBody
-    Event addEvent(@PathVariable String calendarName, @PathVariable String newEvent) {
+    Event addEvent(@PathVariable String calendarName, @PathVariable Event newEvent) {
         if(calendars.containsKey(calendarName)) {
             Calendar updated = calendars.get(calendarName);
             //ADD EVENTS TO CALENDAR
@@ -74,6 +91,7 @@ public class ScheduleController {
         }
         return null;
     }
+    */
 
     /**
      * Returns a boolean representation of whether there are conflicts with the given event
